68c4c75d0ec92eff3688039a538d5a57c085dfe9
Hae-Riri <ehl3288@naver.com>
2023-07-16 01:25:51 +0900

    bugfix: 잘못된 import 해결

--- src/main/java/com/example/cleanarchitecturestudy/account/application/service/GetAccountBalanceService.java
+++ src/main/java/com/example/cleanarchitecturestudy/account/application/service/GetAccountBalanceService.java
@@ -1,17 +1,20 @@
 package com.example.cleanarchitecturestudy.account.application.service;
 
 import com.example.cleanarchitecturestudy.account.application.port.in.GetAccountBalanceQuery;
+import com.example.cleanarchitecturestudy.account.application.port.out.LoadAccountPort;
 import com.example.cleanarchitecturestudy.account.domain.Account;
 import com.example.cleanarchitecturestudy.account.domain.Money;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@RequiredArgsConstructor
 public class GetAccountBalanceService implements GetAccountBalanceQuery {
-    private final LoadAccontPort loadAccontPort;
+    private final LoadAccountPort loadAccountPort;
 
     @Override
     public Money getAccountBalance(Account.AccountId accountId) {
-        return loadAccontPort.loadAccount(accountId, LocalDateTime.now())
+        return loadAccountPort.loadAccount(accountId, LocalDateTime.now())
                 .calculateBalance();
     }
 }
